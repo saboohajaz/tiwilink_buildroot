dc3c39046da08ba84721a7b10aadde6d68e17abd
Scott Ellis <scott@jumpnowtek.com>
2017-01-10 09:58:37 -0500

    Add tspress Qt5 touchscreen test program

---
+++ package/tspress/tspress.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# tspress
+#
+################################################################################
+
+TSPRESS_VERSION = 82a7a614ad858818b0db4d91583a15602c44b42a
+TSPRESS_SITE = git://github.com/scottellis/tspress 
+TSPRESS_LICENSE = MIT
+TSPRESS_LICENSE_FILES = README.md
+
+TSPRESS_DEPENDENCIES = qt5base
+
+define TSPRESS_CONFIGURE_CMDS
+	cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/qmake
+endef
+
+define TSPRESS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define TSPRESS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/tspress $(TARGET_DIR)/usr/bin 
+endef
+
+$(eval $(generic-package))
