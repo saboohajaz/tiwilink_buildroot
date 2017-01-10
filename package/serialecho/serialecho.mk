8dbaa7d2b96a0d7ae290e5a33d03b05c36c43529
Scott Ellis <scott@jumpnowtek.com>
2017-01-10 09:59:43 -0500

    Add serialecho for uart testing

---
+++ package/serialecho/serialecho.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# serialecho 
+#
+################################################################################
+
+SERIALECHO_VERSION = 7acc974041f2e8cb55f892140b306a8c76093811
+SERIALECHO_SITE = git://github.com/scottellis/serialecho 
+SERIALECHO_LICENSE = MIT
+SERIALECHO_LICENSE_FILES = README.md
+
+define SERIALECHO_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define SERIALECHO_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/serialecho $(TARGET_DIR)/usr/bin 
+endef
+
+$(eval $(generic-package))
