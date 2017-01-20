edba38fa560682292342c92637aa4faeca06b024
Scott Ellis <scott@jumpnowtek.com>
2017-01-20 17:42:27 -0500

    rpi-firmware: Upgrade to latest upstream

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 88f75aef50fb2d0165f1bdbe43d7fcf3f5397c02
+RPI_FIRMWARE_VERSION = 7f8ac8dac0b80291cbf5e56580139034a0a42070
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
