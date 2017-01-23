3c527faa8951a1a5500772be471622eff1b95fb6
Scott Ellis <scott@jumpnowtek.com>
2017-01-23 16:07:08 -0500

    pytouch: pull by branch not commit

--- package/pytouch/pytouch.mk
+++ package/pytouch/pytouch.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-PYTOUCH_VERSION = c0d3b2b123e9099d6b0729bf511e428149590656
+PYTOUCH_VERSION = master
 PYTOUCH_SITE = git://github.com/scottellis/pytouch 
 PYTOUCH_LICENSE = MIT
-PYTOUCH_LICENSE_FILES = README.md
 
 PYTOUCH_DEPENDENCIES = python-pyqt5 
 
