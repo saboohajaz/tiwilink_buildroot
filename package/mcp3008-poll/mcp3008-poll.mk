e628014d5a6254dff7cc746291fc55230a656066
Scott Ellis <scott@jumpnowtek.com>
2017-01-27 17:41:41 -0500

    Add mcp3008-poll package

---
+++ package/mcp3008-poll/mcp3008-poll.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# mcp3008-poll
+#
+################################################################################
+
+MCP3008_POLL_VERSION = master
+MCP3008_POLL_SITE = git://github.com/scottellis/mcp3008-poll
+MCP3008_POLL_LICENSE = MIT
+
+define MCP3008_POLL_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define MCP3008_POLL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/mcp3008-poll $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
