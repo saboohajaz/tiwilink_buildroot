949cf9ac990e9c935d75af5b682fe619b1de4d11
Scott Ellis <scott@jumpnowtek.com>
2017-09-15 05:09:01 -0400

    rpi: Upgrade kernel and firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 77d8fdd23ceddfc23e16fa799912645507a7cd62
+RPI_FIRMWARE_VERSION = eb04fcf96b9ff37d00919ce4e1999f802ed8976f
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
