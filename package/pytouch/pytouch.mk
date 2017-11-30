218b3921a80ca5291317203570ed06fe337ad7f9
Scott Ellis <scott@jumpnowtek.com>
2017-11-30 11:35:14 -0500

    pytouch: Use a commit hash for git upstream

--- package/pytouch/pytouch.mk
+++ package/pytouch/pytouch.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTOUCH_VERSION = master
-PYTOUCH_SITE = git://github.com/scottellis/pytouch 
+PYTOUCH_VERSION = 152b8da9f88a674f4e2dcbd9e8b38c4c497241d9
+PYTOUCH_SITE = $(call github,scottellis,pytouch,152b8da9f88a674f4e2dcbd9e8b38c4c497241d9)
 PYTOUCH_LICENSE = MIT
 
 PYTOUCH_DEPENDENCIES = python-pyqt5 
