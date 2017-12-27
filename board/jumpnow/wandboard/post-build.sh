33d98ed2d6ffd440f34e5621395671c7b4e2ace7
Scott Ellis <scott@jumpnowtek.com>
2017-12-27 14:18:10 -0500

    wandboard: Add getty to console

--- board/jumpnow/wandboard/post-build.sh
+++ board/jumpnow/wandboard/post-build.sh
@@ -2,6 +2,13 @@
 
 BOARD_DIR="$(dirname $0)"
 
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+        sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
+
 $HOST_DIR/bin/mkimage -A arm -T script -C none -n "Boot script" \
   -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
 
